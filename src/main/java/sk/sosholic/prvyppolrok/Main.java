e129a2183fe8a54fb57ccaa6c8eaab2da94ec8a6
OndrejF7 <Ondrej.Fusek@sosholic.sk>
2022-09-14 11:17:35 +0200

    Initial commit

---
+++ src/main/java/sk/sosholic/prvyppolrok/Main.java
@@ -0,0 +1,8 @@
+package sk.sosholic.prvyppolrok;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nic som nerobil.");
+        System.out.println("Chodil som na brigadu.");
+    }
+}
---
+++ src/main/java/sk/sosholic/prvyppolrok/opakovaniemavenu.java
@@ -0,0 +1,15 @@
+package sk.sosholic.prvyppolrok;
+
+public class opakovaniemavenu {
+    public static void main(String[] args) {
+        System.out.println("Toto sú primitívne dátové typy");
+        boolean y=true;
+        int i=25;
+        long k=54;
+        float f=88;
+        short s=32;
+        double d=20;
+        byte b=1;
+        char c=3;
+    }
+}
